010e2f138546ebc7cd6c8470b4949ea66b91a9e7
Ona <onushkis@gmail.com>
2022-02-01 00:21:02 +0100

    added changes taiwind

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+     {/* <h1 className='underline text-3xl font-bold text-[#f1356d]'>MONAHOLA</h1> */}
+     <h1  className="border-2 border-rose-500 w-96  text-3xl font-bold text-regal-pink">MONAHOLA</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'regal-pink': '#FFADF0',
+       
+      },
+    },
+  },
+  plugins: [],
+};
+     
+        
+     
